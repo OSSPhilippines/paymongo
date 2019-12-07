b7472ed37c4d63077e9880584b0d00801f70e32c
Jofferson R. Tiquez <jofftiquez@gmail.com>
2019-12-07 16:46:39 +0800

    test: create tester

--- tester.js
+++ tester.js
@@ -1,10 +1,61 @@
-const Paymongo = require('./dist').default;
+// const Paymongo = require('./dist').default;
 
-const paymongo = new Paymongo('sk_test_2h6QPgpJtPtW9Drc47bbaBe4');
+// const paymongo = new Paymongo('sk_test_2h6QPgpJtPtW9Drc47bbaBe4');
 
-paymongo.talk();
-paymongo.meow();
-paymongo.walk();
-paymongo.sleep(3);
-paymongo.feed('fish');
-paymongo.feed('dog food');
+// CREATE TOKEN
+// paymongo.createToken({
+//   data: {
+//     attributes: {
+//       number: '4242424242424242',
+//       exp_month: 1,
+//       exp_year: 23,
+//       cvc: '111'
+//     }
+//   }
+// }).then(res => {
+//   console.log(res);
+// }).catch(error => {
+//   console.log(error);
+// });
+
+// GET TOKEN
+// paymongo.getToken('tok_KudDURimm4f723FcSfMEwU7t').then(res => {
+//   console.log(res);
+// }).catch(error => {
+//   console.log(error);
+// });
+
+// CREATE PAYMENT
+// paymongo.createPayment({
+//   data: {
+//     attributes: {
+//       amount: 10000,
+//       currency: 'PHP',
+//       description: '1 month premium subscription',
+//       statement_descriptor: 'MYCURE Premium',
+//       source: {
+//         id: 'tok_KudDURimm4f723FcSfMEwU7t',
+//         type: 'token'
+//       }
+//     }
+//   }
+// }).then(res => {
+//   console.log(res);
+// }).catch(error => {
+//   console.log(error.message);
+// });
+
+// GET PAYMENT
+// paymongo.getPayment('pay_vLFZMHubC49poHKp5g7M4jfW').then(res => {
+//   console.log(res);
+// }).catch(error => {
+//   console.log(error);
+// });
+
+// GET PAYMENTS
+// paymongo.getPayments().then(res => {
+//   console.log(res);
+// }).catch(error => {
+//   console.log(error);
+// });
+
