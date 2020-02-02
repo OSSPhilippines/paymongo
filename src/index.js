88684fc5bc01891f05e19ed523521b5bb6578513
Jofferson R Tiquez <jofftiquez@gmail.com>
2020-02-02 23:54:00 +0800

    refactor: update class
    - make instance singleton
    - add sources function

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
-import { 
-  createPayment,
-  createToken, 
-  getPayment,
-  getPayments, 
-  getToken
-} from './rest';
+import { createToken, getToken } from './tokens';
+import { createSource } from './sources';
+import { createPayment, getPayment, getPayments } from './payments';
 
 export default class Paymongo {
   constructor (secret) {
-    if (!secret) throw new Error('API is required!');
+    if (Paymongo.instance instanceof Paymongo) return Paymongo.instance;
+    if (!secret) throw new Error('API key is required!');
     this.secret = secret;
+    Object.freeze(this.secret);
+    Object.freeze(this);
+    Paymongo.instance = this;
   }
 
+  // TOKENS
+  async createToken (data) {
+    return createToken(this.secret, data);
+  }
+
+  async getToken (id) {
+    return getToken(this.secret, id);
+  }
+
+  // SOURCES
+  async createSource (data) {
+    return createSource(this.secret, data);
+  }
+
+  // PAYMENTS
   async createPayment (data) {
     return createPayment(this.secret, data);
   }
@@ -20,15 +35,7 @@ export default class Paymongo {
     return getPayment(this.secret, id);
   }
 
-  async getPayments () {
-    return getPayments(this.secret);
-  }
-
-  async getToken (id) {
-    return getToken(this.secret, id);
-  }
-
-  async createToken (data) {
-    return createToken(this.secret, data);
+  async getPayments (data) {
+    return getPayments(this.secret, data);
   }
 }
