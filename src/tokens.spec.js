f0cfa092310f90aa7ba6b72f2528a8209e9a27aa
Jofferson R Tiquez <jofftiquez@gmail.com>
2020-02-03 02:01:05 +0800

    test: create test for specs

---
+++ src/tokens.spec.js
@@ -0,0 +1,44 @@
+import { makeRequest } from './rest';
+import { createToken, getToken } from './tokens';
+
+jest.mock('./rest');
+
+beforeEach(() => {
+  makeRequest.mockClear();
+});
+
+describe('Tokens', () => {
+  describe('|- createToken', () => {
+    makeRequest.mockImplementationOnce(() => Promise.resolve({
+      data: {
+        id: 'tok_123abc'
+      }
+    }));
+
+    it('should return object "data"', async () => {
+      const result = await createToken('fake-private-key', { a: 1 });
+      expect(result).toHaveProperty('data.id');
+    });
+
+    it('should throw', async () => {
+      expect(createToken('fake-private-key', {})).rejects.toEqual(expect.any(Error));
+      expect(createToken('fake-private-key', null)).rejects.toEqual(expect.any(Error));
+      expect(createToken('fake-private-key', undefined)).rejects.toEqual(expect.any(Error));
+      expect(createToken('fake-private-key', new Date())).rejects.toEqual(expect.any(Error));
+      expect(createToken('fake-private-key', [])).rejects.toEqual(expect.any(Error));
+    });
+  });
+
+  describe('|- getToken', () => {
+    it('should return object with id', async () => {
+
+      makeRequest.mockImplementationOnce(() => Promise.resolve({
+        id: 'tok_123abc'
+      }));
+
+      const result = await getToken('fake-private-key', 'tok_123abc');
+      expect(result).toHaveProperty('id');
+      expect(result.id).toEqual('tok_123abc');
+    });
+  })
+});
