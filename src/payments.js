22cf9df7a992b4f66bb2991f73a9029586c8c193
Jofferson R Tiquez <jofftiquez@gmail.com>
2020-02-03 00:00:30 +0800

    chore: add jsdocs

--- src/payments.js
+++ src/payments.js
@@ -1,5 +1,14 @@
 import { makeRequest } from './rest';
 
+/**
+ * These are the required properties
+ * @param  {strong} secret api private key
+ * @param  {Object} data
+ * @property {string} data.attributes.amount amount int32
+ * @property {Object} source the source object from checkout
+ * @property {string} source.id
+ * @property {string} source.type
+ */
 export const createPayment = async (secret, data) => {
   if (!data) throw new Error('Data is required!');
   return makeRequest({
@@ -9,7 +18,10 @@ export const createPayment = async (secret, data) => {
     data
   });
 }
-
+/**
+ * @param  {string} secret api private key
+ * @param  {string} id payment id
+ */
 export const getPayment = async (secret, id) => {
   if (!id) throw new Error('Payment id is required.');
   return makeRequest({
