319403d89c4948a3dfad8a09a62e7e55d77ef141
Jofferson R Tiquez <jofftiquez@gmail.com>
2020-04-12 00:42:07 +0800

    chore: update docs

--- src/sources/sources.js
+++ src/sources/sources.js
@@ -2,11 +2,12 @@ import { makeRequest } from '../utils/rest';
 
 /**
  * These are the required properties
- * @param  {string} secret api private key
- * @param  {Object} data payload
+ * @param {string} secret api private key
+ * @param {Object} data payload
  * @param {Object} data.attributes payload attributes
- * @param {string} data.attributes.type TODO: update this
+ * @param {string} data.attributes.type The type of source. Possible values are gcash and grab_pay.
  * @param {number} data.attributes.amount amount int32
+ * @param {string} data.attributes.currency Three-letter ISO currency code, in uppercase. PHP is the only supported currency as of the moment.
  * @param {Object} data.attributes.redirect
  * @param {string} data.attributes.redirect.success success url
  * @param {string} data.attributes.redirect.failed error url
