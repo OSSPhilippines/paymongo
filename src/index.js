376c7c19a3e60c28550ae05565925f2db8fc9aa5
Jofferson R Tiquez <jofftiquez@gmail.com>
2020-04-12 00:43:25 +0800

    refactor: replace class methods with context variables
    This is to imitate the syntax of stripe payment node library

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+import { createPaymentMethod, retrievePaymentMethod } from './payment-methods/index';
+import { createPaymentIntent, retrievePaymentIntent, attachToPaymentIntent } from './payment-intents/index';
 import { createToken, retrieveToken } from './tokens/index';
 import { createSource } from './sources/index';
-import { createPayment, getPayment, getPayments } from './payments/index';
-import { createWebhook, getWebhooks, toggleWebhook, updateWebhook } from './webhooks/index';
+import { createPayment, retrievePayment, listPayments } from './payments/index';
+import { createWebhook, retrieveWebhook, listWebhooks, toggleWebhook } from './webhooks/index';
 
 export default class Paymongo {
   constructor (secret) {
@@ -9,48 +11,42 @@ export default class Paymongo {
     if (!secret) throw new Error('API key is required!');
     this.secret = secret;
 
+    // PAYMENT METHODS
+    this.paymentMethods = {
+      create: (data) => createPaymentMethod(this.secret, data),
+      retrieve: (id) => retrievePaymentMethod(this.secret, id),
+    };
+    // PAYMENT INTENT
+    this.paymentIntents = {
+      create: (data) => createPaymentIntent(this.secret, data),
+      retrieve: (id) => retrievePaymentIntent(this.secret, id),
+      attach: (id, data) => attachToPaymentIntent(this.secret, id, data),
+    };
+    // SOURCES
+    this.sources = {
+      create: (data) => createSource(this.secret, data),
+    };
+    // PAYMENTS
+    this.payments = {
+      create: (data) => createPayment(this.secret, data),
+      retrieve: (id) => retrievePayment(this.secret, id),
+      list: () => listPayments(this.secret),
+    };
+    // TOKENS
     this.tokens = {
-      create: createToken(this.secret, data),
-      retrieve: retrieveToken(this.secret, id),
+      create: (data) => createToken(this.secret, data),
+      retrieve: (id) => retrieveToken(this.secret, id),
+    };
+    // WEBHOOKS
+    this.webhooks = {
+      create: (data) => createWebhook(this.secret, data),
+      retrieve: (id) => retrieveWebhook(this.secret, id),
+      list: () => listWebhooks(this.secret),
+      toggle: (id, action) => toggleWebhook(this.secret, id, action),
     };
 
     Object.freeze(this.secret);
     Object.freeze(this);
     Paymongo.instance = this;
-  }
-
-  // SOURCES
-  async createSource (data) {
-    return createSource(this.secret, data);
-  }
-
-  // PAYMENTS
-  async createPayment (data) {
-    return createPayment(this.secret, data);
-  }
-
-  async getPayment (id) {
-    return getPayment(this.secret, id);
-  }
-
-  async getPayments (data) {
-    return getPayments(this.secret, data);
-  }
-
-  // WEBHOOKS
-  async createWebhook (data) {
-    return createWebhook(this.secret, data);
-  }
-  
-  async getWebhooks () {
-    return getWebhooks(this.secret);
-  }
-  
-  async toggleWebhook (action, id) {
-    return toggleWebhook(this.secret, action, id);
-  }
-  
-  async updateWebhook (data) {
-    return updateWebhook(this.secret, data);
   }
 }
