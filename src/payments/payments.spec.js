1ee6ff90a19823c9673facec9b2c0c1f2829a33e
Jofferson R Tiquez <jofftiquez@gmail.com>
2020-04-11 10:40:00 +0800

    test(payments): use faker for private key mocking

--- src/payments/payments.spec.js
+++ src/payments/payments.spec.js
@@ -1,5 +1,7 @@
 import { makeRequest } from '../utils/rest';
 import { createPayment, getPayment, getPayments } from './payments';
+import faker from 'faker';
+const fakePrivateKey = faker.random.uuid();
 
 jest.mock('../utils/rest');
 
@@ -14,28 +16,26 @@ describe('Payments', () => {
     }));
 
     it('should return object with id', async () => {
-
-      const result = await createPayment('fake-private-key', { a: 1 });
+      const result = await createPayment(fakePrivateKey, { a: 1 });
       expect(result).toHaveProperty('id');
     });
 
     it('should throw', async () => {
-      expect(createPayment('fake-private-key', {})).rejects.toEqual(expect.any(Error));
-      expect(createPayment('fake-private-key', null)).rejects.toEqual(expect.any(Error));
-      expect(createPayment('fake-private-key', undefined)).rejects.toEqual(expect.any(Error));
-      expect(createPayment('fake-private-key', new Date())).rejects.toEqual(expect.any(Error));
-      expect(createPayment('fake-private-key', [])).rejects.toEqual(expect.any(Error));
+      expect(createPayment(fakePrivateKey, {})).rejects.toEqual(expect.any(Error));
+      expect(createPayment(fakePrivateKey, null)).rejects.toEqual(expect.any(Error));
+      expect(createPayment(fakePrivateKey, undefined)).rejects.toEqual(expect.any(Error));
+      expect(createPayment(fakePrivateKey, new Date())).rejects.toEqual(expect.any(Error));
+      expect(createPayment(fakePrivateKey, [])).rejects.toEqual(expect.any(Error));
     });
   });
 
   describe('|- getPayment', () => {
     it('should return object with id', async () => {
-
       makeRequest.mockImplementationOnce(() => Promise.resolve({
         id: 'pay_123abc',
       }));
 
-      const result = await getPayment('fake-private-key', 'pay_123abc');
+      const result = await getPayment(fakePrivateKey, 'pay_123abc');
       expect(result).toHaveProperty('id');
       expect(result.id).toEqual('pay_123abc');
     });
@@ -43,12 +43,11 @@ describe('Payments', () => {
   
   describe('|- getPayments', () => {
     it('should return data[]', async () => {
-
       makeRequest.mockImplementationOnce(() => Promise.resolve({
         data: [],
       }));
 
-      const result = await getPayments('fake-private-key', 'pay_123abc');
+      const result = await getPayments(fakePrivateKey, 'pay_123abc');
       expect(Array.isArray(result.data)).toBe(true);
     });
   });
