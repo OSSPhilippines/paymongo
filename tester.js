16843739738e33ec89add416502bb12b2274817a
Jofferson R Tiquez <jofftiquez@gmail.com>
2020-04-12 00:40:01 +0800

    chore: update tester.js

--- tester.js
+++ tester.js
@@ -2,59 +2,152 @@
 
 // const paymongo = new Paymongo('sk_test_2h6QPgpJtPtW9Drc47bbaBe4');
 
-// CREATE TOKEN
-// paymongo.createToken({
+// PAYMENT METHODS
+// paymongo.paymentMethods.create({
 //   data: {
 //     attributes: {
-//       number: '4242424242424242',
-//       exp_month: 1,
-//       exp_year: 23,
-//       cvc: '111'
-//     }
-//   }
-// }).then(res => {
-//   console.log(res);
-// }).catch(error => {
-//   console.log(error);
-// });
-
-// GET TOKEN
-// paymongo.getToken('tok_KudDURimm4f723FcSfMEwU7t').then(res => {
-//   console.log(res);
-// }).catch(error => {
-//   console.log(error);
-// });
-
-// CREATE PAYMENT
-// paymongo.createPayment({
-//   data: {
-//     attributes: {
-//       amount: 10000,
-//       currency: 'PHP',
-//       description: '1 month premium subscription',
-//       statement_descriptor: 'MYCURE Premium',
-//       source: {
-//         id: 'tok_KudDURimm4f723FcSfMEwU7t',
-//         type: 'token'
+//       type: 'card',
+//       details: {
+//         card_number: '4343434343434345',
+//         exp_month: 1,
+//         exp_year: 23,
+//         cvc: '111',
+//       },
+//       metadata: {
+//         joff: 'pogi'
 //       }
 //     }
 //   }
 // }).then(res => {
 //   console.log(res);
-// }).catch(error => {
-//   console.log(error.message);
+// }).catch(err => {
+//   console.log(err);
 // });
 
-// GET PAYMENT
-// paymongo.getPayment('pay_vLFZMHubC49poHKp5g7M4jfW').then(res => {
-//   console.log(res);
-// }).catch(error => {
-//   console.log(error);
+// paymongo.paymentMethods.retrieve('pm_JPo6NDxnNnd8xX8DHnzgocqK')
+// .then(res => {
+//   console.log(res.data.attributes);
+// }).catch(err => {
+//   console.log(err);
 // });
 
-// GET PAYMENTS
-// paymongo.getPayments().then(res => {
+// PAYMENT INTENTS
+
+// paymongo.paymentIntents.create({
+//   data: {
+//     attributes: {
+//       amount: 10000,
+//       payment_method_allowed: ['card'],
+//       currency: 'PHP'
+//     }
+//   }
+// }).then(res => {
 //   console.log(res);
-// }).catch(error => {
-//   console.log(error);
+// }).catch(err => {
+//   console.log(err);
+// });
+
+// paymongo.paymentIntents.retrieve('pi_JeH1tVbD4bjk97nbJfyoHQMs')
+// .then(res => {
+//   console.log(res.data.attributes);
+// }).catch(err => {
+//   console.log(err);
+// });
+
+// paymongo.paymentIntents.attach('pi_JeH1tVbD4bjk97nbJfyoHQMs', {
+//   data: {
+//     attributes: {
+//       payment_method: 'pm_JPo6NDxnNnd8xX8DHnzgocqK',
+//     }
+//   }
+// }).then(res => {
+//   console.log(res);
+// }).catch(err => {
+//   console.log(err);
+// });
+
+// SOURCES
+
+// paymongo.sources.create({
+//   data: {
+//     attributes: {
+//       type: 'gcash',
+//       amount: 10000,
+//       currency: 'PHP',
+//       redirect: {
+//         success: 'https://mycure.md',
+//         failed: 'https://facebook.com'
+//       }
+//     }
+//   }
+// }).then(res => {
+//   console.log(res);
+// }).catch(err => {
+//   console.log(err);
+// });
+
+// PAYMENTS
+
+// paymongo.payments.create({
+//   data: {
+//     attributes: {
+//       amount: 10000,
+//       currency: 'PHP',
+//       source: {
+//         id: 'src_wL2f1ej2Ci7CuYxUJy8Gwj5j',
+//         type: 'source',
+//       }
+//     }
+//   }
+// }).then(res => {
+//   console.log(res);
+// }).catch(err => {
+//   console.log(err);
+// });
+
+// paymongo.payments.retrieve('pay_4f983Gw6eg48GY14TjmwGMVC')
+// .then(res => {
+//   console.log(res);
+// }).catch(err => {
+//   console.log(err);
+// });
+
+// paymongo.payments.list()
+// .then(res => {
+//   console.log(res);
+// }).catch(err => {
+//   console.log(err);
+// });
+
+// WEBHOOKS
+
+// paymongo.webhooks.create({
+//   data: {
+//     attributes: {
+//       url: 'https://mycure.md/foo',
+//       events: ['source.chargeable']
+//     }
+//   }
+// }).then(res => {
+//   console.log(res);
+// }).catch(err => {
+//   console.log(err);
+// });
+
+// paymongo.webhooks.retrieve('hook_zQQ2ALLh6bd5EfTWg5wfytFn').then(res => {
+//   console.log(res);
+// }).catch(err => {
+//   console.log(err);
+// });
+
+// paymongo.webhooks.list().then(res => {
+//   console.log(res);
+// }).catch(err => {
+//   console.log(err);
+// });
+
+// paymongo.webhooks.toggle('hook_zQQ2ALLh6bd5EfTWg5wfytFn', 'enable').then(res => {
+//   console.log(res);
+// }).catch(err => {
+//   console.log(err);
 // });
