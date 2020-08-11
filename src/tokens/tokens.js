de307c6cbd6311b33b792830e86a2ef16e9f58c9
Jofferson R Tiquez <jofftiquez@gmail.com>
2020-08-11 21:03:51 +0800

    docs: update jsdocs
    - add deprecation note

--- src/tokens/tokens.js
+++ src/tokens/tokens.js
@@ -2,13 +2,14 @@ import { makeRequest } from '../utils/rest';
 
 /**
  * These are the required properties
- * @param {string} secret api private key
- * @param {Object} data payload
- * @param {Object} data.attributes payload attributes
- * @param {string} data.attributes.number credit card number
- * @param {number} data.attributes.exp_month credit card expiry month `float`
- * @param {number} data.attributes.exp_year credit card expiry year `float`
- * @param {string} data.attributes.cvc credit card cvc
+ * @deprecated Tokens are now deprecated. https://developers.paymongo.com/reference#token-resource
+ * @param {string} secret API private key
+ * @param {Object} data Data payload
+ * @param {Object} data.attributes Payload attributes
+ * @param {string} data.attributes.number Credit card number
+ * @param {number} data.attributes.exp_month Credit card expiry month `float`
+ * @param {number} data.attributes.exp_year Credit card expiry year `float`
+ * @param {string} data.attributes.cvc Credit card cvc
  */
 export const createToken = async (secret, data) => {
   console.warn('Deprecated: Tokens are now deprecated. https://developers.paymongo.com/reference#token-resource');
@@ -23,6 +24,7 @@ export const createToken = async (secret, data) => {
 };
 
 /**
+ * @deprecated Tokens are now deprecated. https://developers.paymongo.com/reference#token-resource
  * @param {string} secret api private key
  * @param {string} id token id
  */
